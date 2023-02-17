6a3fd25dccc69769b16f9397533bf5af5bf202c2
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-02-17 14:32:41 +0200

    organising

---
+++ MainClass.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace A1_CS251 {
+    public class MainClass {
+        
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
