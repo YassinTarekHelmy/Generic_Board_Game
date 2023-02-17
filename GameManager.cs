9cdd41911e64bd38679bd765fcd4ae5461b944cb
YassinTarekHelmy <11410120210450@stud.cu.edu.eg>
2023-02-17 21:03:56 +0200

    NOUR 3YEL kwayes

--- GameManager.cs
+++ GameManager.cs
@@ -1,20 +1,26 @@
 namespace A1_CS251 {
     public class GameManager {
         // static int x, y;
+         bool gameIsRunning = true;
         Player[] player = new Player[2];
         Board gameBoard = new Board(0, 0);
         IGameLogic? gameLogic;
         public void Run() {
-            gameLogic = new XO_GameLogic(ref gameBoard);
-            
-            player[0] = new Player('1', 'y', gameLogic);
-            player[1] = new Computer('x', gameLogic);
+            gameLogic = new XO_GameLogic(ref gameBoard);  
+            player[0] = new Player('1', 'o', gameLogic);
+            player[1] = new Player('2','x', gameLogic);
             Console.WriteLine(player[1].GetName());
             // gameBoard = new Board(3, 3);
-            while(true) {
-                if (gameLogic.IsWinner(gameBoard)){
-                    gameLogic.DisplayBoard(gameBoard);
-                    break;
+            while(gameIsRunning) {
+                gameLogic.DisplayBoard(gameBoard);
+                foreach (Player P in player){
+                    P.GetMove();
+                    if (gameLogic.IsWinner(gameBoard)){
+                        Console.WriteLine(P.GetName() + " is Winner!!"); 
+                        gameLogic.DisplayBoard(gameBoard);  //board is displayed for the last time.
+                        gameIsRunning = false;
+                        break;
+                    }
                 }
             }
         }
--- MainClass.cs
+++ MainClass.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace A1_CS251 {
     public class MainClass {
+        
         public static void Main(string[] args) {
             GameManager game = new GameManager();
             game.Run();
