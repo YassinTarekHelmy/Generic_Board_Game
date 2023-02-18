ccc2bbfedebe6b858efabf311047daa6fec958b0
Nour8317 <noure8317@gmail.com>
2023-02-18 17:09:30 +0200

    is valid move & display board

--- Connect4_GameLogic.cs
+++ Connect4_GameLogic.cs
@@ -1,16 +1,28 @@
 namespace A1_CS251 {
     public class Connect4_GameLogic : IGameLogic {
+        int colomn;
         public void ComputerMove(Board board, char symbol)
         {
             throw new NotImplementedException();
         }
 
-        public void DisplayBoard(Board board) {
-            throw new NotImplementedException();
+         public void DisplayBoard(Board board) {
+            for (int i = 0; i < board.GetLength(); i++){
+                System.Console.Write("| {0} |", i);
+            }
+            System.Console.WriteLine();
+            for (int i = 0; i < board.GetWidth(); i++){
+                for (int j = 0; j < board.GetLength(); j++){
+                    Console.Write("| {0} |", board.GetPosition(i,j));
+                }
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine("----------------------");
         }
 
         public void GetMove() {
-            throw new NotImplementedException();
+            Console.Write("Enter X position: ");
+            colomn = Convert.ToInt32(Console.ReadLine());
         }
 
         public bool IsDraw(Board board) {
@@ -18,7 +30,15 @@ namespace A1_CS251 {
         }
 
         public bool IsValidMove(ref Board board, char symbol) {
-            throw new NotImplementedException();
+            for (int i = board.GetWidth()-1; i >= 0; i--)
+            {
+                if (board.GetPosition(colomn , i) == '.')
+                {
+                    board.UpdateBoard(colomn, i, symbol);
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsWinner(Board board) {
